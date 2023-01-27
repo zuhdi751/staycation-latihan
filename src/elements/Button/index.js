f9d2477ab2b8dd48bc29e466d4af398574126c88
zuhdi751 <ahmadzuhdi751@gmail.com>
2023-01-27 10:01:14 +0700

    button, install proptypes, dan  set propTypes pada button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import propTypes from 'prop-types'
 
 function Button(props) {
   return (
@@ -10,3 +12,18 @@ function Button(props) {
 
 export default Button
 
+Button.propTypes = {
+  type: propTypes.oneOf(['button', 'link']),
+  onClick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternall: propTypes.bool,
+  hasShadow: propTypes.bool,
+}
+
+// taget itu untuk link externall, isExternall itu untuk memastikan apakah link itu mengarah ke luar aplikasi atau ke dalam aplikasi (jika luar pakai tag a, jika di dalam aplikasi pakai link dari react-router-dom)
