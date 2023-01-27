eca0bfcb6ea727d5b3815f867c2c251d3ccb6e1c
zuhdi751 <ahmadzuhdi751@gmail.com>
2023-01-27 10:26:44 +0700

    implementas dari propsType tadi, rendering Components, terahir render di button link internal dan external, next return button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,21 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
 function Button(props) {
-  return (
-    <div>
-      
-    </div>
-  )
+  // implementasi dari props yang kita terima tadi
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  // rendering component
+  if (props.type === "link") {
+    if (props.isExternall) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
+  return <div></div>;
 }
 
-export default Button
+export default Button;
 
 Button.propTypes = {
-  type: propTypes.oneOf(['button', 'link']),
+  type: propTypes.oneOf(["button", "link"]),
   onClick: propTypes.func,
   target: propTypes.string,
+  href: propTypes.string,
   className: propTypes.string,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
@@ -24,6 +61,6 @@ Button.propTypes = {
   isBlock: propTypes.bool,
   isExternall: propTypes.bool,
   hasShadow: propTypes.bool,
-}
+};
 
 // taget itu untuk link externall, isExternall itu untuk memastikan apakah link itu mengarah ke luar aplikasi atau ke dalam aplikasi (jika luar pakai tag a, jika di dalam aplikasi pakai link dari react-router-dom)
